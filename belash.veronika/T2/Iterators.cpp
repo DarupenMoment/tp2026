e8759f6388f88cc9818449bf7869ed1a45a00df5
NiKaaa09 <Veronika_200929@mail.ru>
2026-04-01 14:25:39 +0300

    Replacing Iterators.cpp

--- belash.veronika/T2/Iterators.cpp
+++ belash.veronika/T2/Iterators.cpp
@@ -7,13 +7,15 @@
 #include <iomanip>
 #include <cctype>
 
-struct DataStruct {
+struct DataStruct
+{
     double key1;
     unsigned long long key2;
     std::string key3;
 };
 
-std::string extractValue(const std::string& line, const std::string& key) {
+std::string extractValue(const std::string& line, const std::string& key)
+{
     std::string search = ":" + key + " ";
     size_t pos = line.find(search);
     if (pos == std::string::npos) return "";
@@ -23,7 +25,8 @@ std::string extractValue(const std::string& line, const std::string& key) {
     return line.substr(pos, end - pos);
 }
 
-bool parseDataStruct(const std::string& line, DataStruct& ds) {
+bool parseDataStruct(const std::string& line, DataStruct& ds)
+{
     if (line.size() < 4 || line[0] != '(' || line[1] != ':' ||
         line[line.size() - 2] != ':' || line[line.size() - 1] != ')')
         return false;
@@ -34,7 +37,6 @@ bool parseDataStruct(const std::string& line, DataStruct& ds) {
     if (key1_val.empty() || key2_val.empty() || key3_val.empty())
         return false;
 
-    // key1: double с суффиксом d/D
     std::istringstream iss1(key1_val);
     double d;
     if (!(iss1 >> d)) return false;
@@ -42,10 +44,9 @@ bool parseDataStruct(const std::string& line, DataStruct& ds) {
     if (!(iss1 >> suffix)) return false;
     if (suffix != 'd' && suffix != 'D') return false;
     std::string leftover1;
-    if (iss1 >> leftover1) return false; // не должно быть лишних символов
+    if (iss1 >> leftover1) return false;
     ds.key1 = d;
 
-    // key2: unsigned long long с суффиксом u11/ULL
     std::istringstream iss2(key2_val);
     unsigned long long ull;
     if (!(iss2 >> ull)) return false;
@@ -57,7 +58,6 @@ bool parseDataStruct(const std::string& line, DataStruct& ds) {
     if (iss2 >> leftover2) return false;
     ds.key2 = ull;
 
-    // key3: строка в двойных кавычках
     if (key3_val.size() < 2 || key3_val.front() != '"' || key3_val.back() != '"')
         return false;
     ds.key3 = key3_val.substr(1, key3_val.size() - 2);
@@ -65,31 +65,44 @@ bool parseDataStruct(const std::string& line, DataStruct& ds) {
     return true;
 }
 
-int main() {
+int main()
+{
     std::vector<DataStruct> vec;
     std::string line;
 
-    while (std::getline(std::cin, line)) {
+    while (std::getline(std::cin, line))
+    {
         DataStruct ds;
-        if (parseDataStruct(line, ds)) {
+        if (parseDataStruct(line, ds))
+        {
             vec.push_back(ds);
         }
     }
 
-    if (vec.empty()) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
-    }
-    else {
-        std::sort(vec.begin(), vec.end(),
-            [](const DataStruct& a, const DataStruct& b) {
-                if (a.key1 != b.key1) return a.key1 < b.key1;
-                if (a.key2 != b.key2) return a.key2 < b.key2;
-                return a.key3.size() < b.key3.size();
-            });
+    std::sort(vec.begin(), vec.end(),
+        [](const DataStruct& a, const DataStruct& b)
+        {
+            if (a.key1 != b.key1) return a.key1 < b.key1;
+            if (a.key2 != b.key2) return a.key2 < b.key2;
+            return a.key3.size() < b.key3.size();
+        });
 
-        // Для прохождения теста выводим сообщение
-        std::cout << "Atleast one supported record type" << std::endl;
+    if (vec.empty())
+    {
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+    }
+    else
+    {
+        std::copy(vec.begin(), vec.end(),
+            std::ostream_iterator<DataStruct>(std::cout, "\n"));
     }
 
     return 0;
+}
+
+std::ostream& operator<<(std::ostream& out, const DataStruct& src)
+{
+    out << "(:key1 " << std::fixed << std::setprecision(1) << src.key1 << "d:key2 "
+        << src.key2 << "u11:key3 \"" << src.key3 << "\":)";
+    return out;
 }
