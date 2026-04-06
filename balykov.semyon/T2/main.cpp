8586211c016c13773aa1c80a54ad868d0a5ea4bd
Balykov Semyon <balykov.semen2010@mail.ru>
2026-04-06 23:27:39 +0300

    Fix line endings globally

--- balykov.semyon/T2/main.cpp
+++ balykov.semyon/T2/main.cpp
@@ -52,8 +52,7 @@ std::istream& operator>>(std::istream& in,
     ULLBinIO&& dest);
 std::istream& operator>>(std::istream& in,
     ComplexIO&& dest);
-std::istream& operator>>(std::istream& in, 
-    StringIO&& dest);
+std::istream& operator>>(std::istream& in, StringIO&& dest);
 std::istream& operator>>(std::istream& in, 
     DataStruct& dest);
 std::ostream& operator<<(std::ostream& out, 
